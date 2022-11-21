259881b5f02305025f1bf106f0e54d9f20931dd7
Ege Gündüz <m.egegunduz@gmail.com>
2022-11-21 18:02:13 +0300

    splash screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { MainNavigation } from './Navigation';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './Redux/Store';
 import { LoadingOverlay } from './Features/Loading';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = props => {
+    useEffect(() => {
+        SplashScreen.hide();
+    }, []);
+
     return (
         <Provider store={store}>
             <PersistGate persistor={persistor}>
